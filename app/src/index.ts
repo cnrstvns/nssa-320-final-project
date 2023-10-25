22d81d5ac912d4310d7497cd6f0d4e2db0d23cb5
Connor Stevens <conmail1224@gmail.com>
2023-10-24 23:56:17 -0400

    Add prettier config

--- app/src/index.ts
+++ app/src/index.ts
@@ -1,13 +1,13 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get("/", (_req, res) => {
-  res.send("Hello world!");
+app.get('/', (_req, res) => {
+  res.send('Hello world!');
 });
 
 app.listen(port, () => {
